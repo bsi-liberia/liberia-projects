f46d52e9355779e43f9be0eb789793ff8bb5465c
Mark Brough <mark.brough@gmail.com>
2016-03-01 01:37:46 +0100

    Add various codelists and functions to access them

---
+++ maediprojects/lib/codelists.py
@@ -0,0 +1,35 @@
+import unicodecsv
+import os
+from maediprojects import app, db, models
+LANG = app.config["LANG"]
+
+def get_db_codelists():
+    # Get codelist name
+    # { codelist: cdata }
+    codelist_data = models.CodelistCode.query.all()
+    codelists = dict(map(lambda k: (k.codelist_code, []), codelist_data))
+    for cd in codelist_data:
+        codelists[cd.codelist_code].append({
+            "code": cd.code,
+            "name": cd.name
+        })
+    return codelists
+
+def get_codelists():
+    current_dir = os.path.join(os.path.dirname(__file__))
+    
+    def only_csv(filename):
+        if filename.endswith(".csv"): return True
+        return False
+    
+    codelists = filter(only_csv, os.listdir(os.path.join(current_dir, "data")))
+    out = {}
+    for codelist in codelists:
+        cl_name = codelist.split(".")[0]
+        out[cl_name] = []
+        cl_file = open(os.path.join(current_dir, "data", codelist), "r")
+        csv = unicodecsv.DictReader(cl_file)
+        for row in csv:
+            out[cl_name].append({"code": row["code"], "name": row["name_%s" % LANG]})
+    out.update(get_db_codelists())
+    return out
