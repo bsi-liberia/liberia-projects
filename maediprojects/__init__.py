e69d1d2d270f045d6ac21e0e3ba8a7a1e300e7e2
Mark Brough <mark.brough@gmail.com>
2016-03-03 13:07:46 +0100

    Setup babel

--- maediprojects/__init__.py
+++ maediprojects/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.babel import Babel
 import os
 
 app = Flask(__name__.split('.')[0])
 app.config.from_pyfile(os.path.join('..', 'config.py'))
 db = SQLAlchemy(app)
+babel = Babel(app)
 
 import routes
+
+@babel.localeselector
+def get_locale():
+    return app.config["BABEL_DEFAULT_LOCALE"]
