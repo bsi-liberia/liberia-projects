fdd4beed0fbeec249260adee92c3abb203b339fe
Mark Brough <mark.brough@gmail.com>
2016-05-05 18:29:09 +0200

    Return list of countries

--- maediprojects/views/activities.py
+++ maediprojects/views/activities.py
@@ -10,7 +10,9 @@ import json, datetime
 
 @app.route("/")
 def dashboard():
+    countries = qactivity.get_iati_list()
     return render_template("home.html",
+                countries=countries,
                 loggedinuser=current_user,
                 stats = qactivity.get_stats(current_user),
                 activities = qactivity.list_activities_user(current_user)
