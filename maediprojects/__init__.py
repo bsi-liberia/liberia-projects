852f067c7aab6bdcaabf2550fc5a0995a7e9b0ae
Mark Brough <mark.brough@gmail.com>
2016-05-07 12:28:05 +0200

    Add flask-mail to use for emailing updates

--- maediprojects/__init__.py
+++ maediprojects/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.babel import Babel
+from flask.ext.mail import Mail
 import os
 
 app = Flask(__name__.split('.')[0])
 app.config.from_pyfile(os.path.join('..', 'config.py'))
 db = SQLAlchemy(app)
 babel = Babel(app)
+mail = Mail(app)
 
 import routes
 
