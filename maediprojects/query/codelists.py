46157b01310e24c7dd635fcaac0d55222c28cd26
Mark Brough <mark.brough@gmail.com>
2018-06-13 18:18:55 +0200

    Adjustments to the way codelists work

--- maediprojects/query/codelists.py
+++ maediprojects/query/codelists.py
@@ -1,5 +1,23 @@
 from maediprojects import db, models
 import datetime
+import normality
+
+def get_code_by_name(codelist, name):
+    code = models.CodelistCode.query.filter_by(
+        codelist_code = codelist,
+    	name = name
+    ).first()
+    if code: return code
+    return False
+
+def get_or_create_code(codelist, name):
+    code = get_code_by_name(codelist, name)
+    if code: return code.code
+    return create_code({
+        "codelist_code": codelist,
+        "name": name,
+        "code": normality.slugify(name)
+    }).code
 
 def create_code(data):
     codelistcode = models.CodelistCode()
@@ -16,6 +34,12 @@ def update_attr(data):
     	codelist_code = data["codelist_code"]
     ).first()
     setattr(codelistcode, data['attr'], data['value'])
+    """
+    if data['codelist_code'] == 'funding-organisation':
+        models.Activity.query.filter_by(
+            funding_org_code
+        )
+    """
     db.session.add(codelistcode)
     db.session.commit()
     return True
@@ -24,7 +48,7 @@ def delete_code(data):
     check = models.Activity.query.filter((
         models.Activity.executing_org == data['code']
     ) | (
-        models.Activity.cicid_sector == data['code']
+        models.Activity.local_sector == data['code']
     )).first()
     if check: return False
     codelistcode = models.CodelistCode.query.filter_by(
