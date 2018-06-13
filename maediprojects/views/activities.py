55d36fe5c5964f7d8c6da285ec3f6399512e59bb
Mark Brough <mark.brough@gmail.com>
2018-06-13 18:17:47 +0200

    Add some additional routes for activities view

--- maediprojects/views/activities.py
+++ maediprojects/views/activities.py
@@ -13,8 +13,14 @@ import json, datetime
 @login_required
 def dashboard():
     countries = qactivity.get_iati_list()
+    funding_orgs = codelists.get_codelists_lookups_by_name()["funding-organisation"]
+    mtef_sectors = codelists.get_codelists_lookups_by_name()["mtef-sector"]
+    aligned_ministry_agencies = codelists.get_codelists_lookups_by_name()["aligned-ministry-agency"]
     return render_template("home.html",
                 countries=countries,
+                funding_orgs=sorted(funding_orgs.items()),
+                mtef_sectors=sorted(mtef_sectors.items()),
+                aligned_ministry_agencies=sorted(aligned_ministry_agencies.items()),
                 loggedinuser=current_user,
                 stats = qactivity.get_stats(current_user)
                           )
@@ -39,6 +45,7 @@ def activity_new():
                         "aid_type": "C01",
                         "collaboration_type": "1",
                         "finance_type": "110",
+                        "activity_status": "2",
                         "tied_status": "5",
                         "start_date": today,
                         "end_date": today,
@@ -76,6 +83,7 @@ def activity_edit(activity_id):
     activity = qactivity.get_activity(activity_id)
     locations = qlocation.get_locations_country(
                                     activity.recipient_country_code)
+    #FIXME why are these not url_for()s ?
     return render_template("activity_edit.html",
                 activity = activity,
                 loggedinuser=current_user,
@@ -85,6 +93,8 @@ def activity_edit(activity_id):
                 api_activity_locations_url = "/api/activity_locations/%s/" % activity_id,
                 api_activity_finances_url = "/api/activity_finances/%s/" % activity_id,
                 api_update_activity_finances_url = "/api/activity_finances/%s/update_finances/" % activity_id,
+                api_iati_search_url = "/api/iati_search/",
+                api_activity_forwardspends_url = url_for("api_activity_forwardspends", activity_id=activity_id),
                 users = quser.user()
           )
 
