96a39c50d96bc480c677d49148acf6b215fd6878
Mark Brough <mark.brough@gmail.com>
2018-06-13 18:08:27 +0200

    Add some more API functions, including to allow for searching IATI for project data

--- maediprojects/views/api.py
+++ maediprojects/views/api.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, flash, request, Markup, \
     session, redirect, url_for, escape, Response, abort, send_file, \
     jsonify, current_app
 from flask.ext.login import login_required, current_user
+from sqlalchemy.sql import func
                             
 from maediprojects import app, db, models
 from maediprojects.query import activity as qactivity
@@ -12,7 +13,11 @@ from maediprojects.query import generate as qgenerate
 from maediprojects.query import generate_csv as qgenerate_csv
 from maediprojects.query import generate_xlsx as qgenerate_xlsx
 from maediprojects.lib import codelists
-import datetime, json
+from maediprojects.lib.util import MONTHS_QUARTERS, QUARTERS_MONTH_DAY
+import requests
+import datetime, json, collections
+
+OIPA_SEARCH_URL = "https://www.oipa.nl/api/activities/?format=json&q=%22{}%22&recipient_country=LR&reporting_organisation_identifier={}"
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -27,15 +32,28 @@ def jsonify(*args, **kwargs):
 
 @app.route("/api/activities/", methods=["POST", "GET"])
 def api_activities_country():
-    if "country_code" not in request.form:
-        activities = qactivity.list_activities_user(current_user)
-    else:
+    if "country_code" in request.form:
         activities = qactivity.list_activities_by_country(
             request.form["country_code"])
+    elif "funding_org_code" in request.form:
+        activities = qactivity.list_activities_by_filter(
+            "funding_org_code",
+            request.form["funding_org_code"])
+    elif "mtef_sector" in request.form:
+        activities = qactivity.list_activities_by_filter(
+            "mtef-sector",
+            request.form["mtef_sector"])
+    elif "aligned_ministry_agency" in request.form:
+        activities = qactivity.list_activities_by_filter(
+            "aligned-ministry-agency",
+            request.form["aligned_ministry_agency"])
+    else:
+        activities = qactivity.list_activities_user(current_user)
 
     return jsonify(activities = [{
         'title': activity.title,
         'country': activity.recipient_country.name,
+        'funding_org': activity.funding_org.name,
         'id': activity.id,
         'updated_date': activity.updated_date.date().isoformat(),
         'activity_edit_url': url_for('activity_edit', 
@@ -70,6 +88,54 @@ def api_activity_finances(activity_id):
 @app.route("/api/activity_finances/<activity_id>/update_finances/", methods=['POST'])
 @login_required
 def finances_edit_attr(activity_id):
+    data = {
+        'attr': request.form['attr'],
+        'value': request.form['value'],
+        'finances_id': request.form['finances_id'],
+    }
+    update_status = qfinances.update_attr(data)
+    if update_status == True:
+        return "success"
+    return "error"
+
+@app.route("/api/activity_forwardspends/<activity_id>/", methods=["GET", "POST"])
+@login_required
+def api_activity_forwardspends(activity_id):
+    """GET returns a list of all forward spend data for a given activity_id.
+    
+    POST updates value for a given forwardspend_id."""
+
+    if request.method == "GET":
+        data = qactivity.get_activity(activity_id).forwardspends
+        forwardspends = list(map(lambda fs_db: fs_db.as_dict(),
+                         qactivity.get_activity(activity_id).forwardspends))
+        years = sorted(set(map(lambda fs: fs["value_date"].year, 
+                         forwardspends)))
+        out = collections.OrderedDict()
+        for year in years:
+            out[year] = {"year": year, "total_value": 0.00}
+            for forwardspend in forwardspends:
+                if forwardspend["period_start_date"].year == year:
+                    fq = MONTHS_QUARTERS[forwardspend["period_start_date"].month]
+                    out[year]["Q{}".format(fq)] = forwardspend
+                    out[year]["total_value"] += float(forwardspend["value"])
+        out = list(out.values())
+        return jsonify(forwardspends=out)
+
+    elif request.method == "POST":
+
+        data = {
+            "id": request.form["id"],
+            "value": request.form["value"]
+        }
+        update_status = qfinances.update_fs_attr(data)
+        if update_status == True:
+            return "success"
+        return "error"
+
+@app.route("/api/activity_forwardspends/<activity_id>/update_forwardspends/", methods=['POST'])
+@login_required
+def forwardspends_edit_attr(activity_id):
     data = {
         'attr': request.form['attr'],
         'value': request.form['value'],
@@ -154,6 +220,39 @@ def api_list_routes():
 def api_list_iati_files():
     urls = qactivity.get_iati_list()
     return jsonify(urls = urls)
+
+@app.route("/api/iati_search/", methods=["POST"])
+def api_iati_search():
+    title = request.form["title"]
+    reporting_org_code = request.form["reporting_org_code"]
+    
+    r = requests.get(OIPA_SEARCH_URL.format(title, reporting_org_code))
+    data = json.loads(r.text)
+    return jsonify(data)
+
+@app.route("/api/sectors.json")
+def api_sectors():
+    sector_totals = db.session.query(
+        func.sum(models.ActivityFinances.transaction_value).label("total_disbursement"),
+        models.CodelistCode.code,
+        models.CodelistCode.name
+    ).join(
+        models.Activity,
+        models.ActivityCodelistCode,
+        models.CodelistCode
+    ).filter(
+        models.ActivityFinances.transaction_type == "D",
+        models.ActivityFinances.transaction_date>datetime.datetime(2017,7,1),
+        models.CodelistCode.codelist_code == "mtef-sector"
+    ).group_by(
+        models.CodelistCode.name,
+        models.CodelistCode.code
+    ).all()
+    return jsonify(sectors = list(map(lambda s: {
+        "name": s.name,
+        "value": s.total_disbursement,
+        "code": s.code
+    }, sector_totals)))
 
 @app.route("/api/iati/<version>/<country_code>.xml")
 def generate_iati_xml(version, country_code):
