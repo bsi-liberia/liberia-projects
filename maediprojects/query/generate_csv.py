441f2bc6d658503db3856a81b3944f824b3c3798
Mark Brough <mark.brough@gmail.com>
2018-06-25 00:15:11 +0200

    Update CSV file for MFDP

--- maediprojects/query/generate_csv.py
+++ maediprojects/query/generate_csv.py
@@ -34,11 +34,10 @@ def valid_transaction(transaction):
 
 def activity_to_json(activity, cl_lookups):
     activity_commitments = filter(valid_transaction, activity.commitments)
-    sum_commitments = {True: sum(map(lambda c: c.transaction_value, activity_commitments)),
-          False: activity.total_commitments}[len(activity_commitments) > 0]
+    sum_commitments = sum(map(lambda c: c.transaction_value, activity_commitments))
+    
     activity_disbursements = filter(valid_transaction, activity.disbursements)
-    sum_disbursements = {True: sum(map(lambda d: d.transaction_value, activity_disbursements)),
-          False: activity.total_disbursements}[len(activity_disbursements) > 0]
+    sum_disbursements = sum(map(lambda d: d.transaction_value, activity_disbursements))
           
     longs = list(map(lambda l: float(l.locations.longitude), activity.locations))
     lats = list(map(lambda l: float(l.locations.latitude), activity.locations))
@@ -50,11 +49,8 @@ def activity_to_json(activity, cl_lookups):
         average_longs = ""
         average_lats = ""
     
-    return {u'Reporting Organisation': "MAEDI",
-     u'Standard activity identifier': { True: activity.code, 
-               False: activity.id
-             }[bool(activity.code)], 
-        u'Other activity identifiers': { True: activity.code, 
+    return {u'Reporting Organisation': "MFDP",
+     u'Project code': { True: activity.code, 
                False: activity.id
              }[bool(activity.code)],
         u'Activity Title': re.sub("\t|\n|\r", "", activity.title), 
@@ -65,38 +61,31 @@ def activity_to_json(activity, cl_lookups):
         u'Activity Dates (Start Date)': activity.start_date.isoformat() if activity.start_date else "",
         u'Activity Dates (End Date)': activity.end_date.isoformat() if activity.end_date else "", 
         u'Activity Contacts': "",
-        u'Participating Organisation (Funding)': "France", 
+        u'Participating Organisation (Funding)': activity.funding_org.name, 
         u'Participating Organisation (Implementing)': activity.implementing_org,
         u'Recipient Country': cl_lookups["Country"][activity.recipient_country_code], 
         u'Recipient Region': "",
         u'Sub-national Geographic Location - Latitude': average_lats, 
         u'Sub-national Geographic Location - Longitude': average_longs,
         u'Sector (DAC CRS)': cl_lookups["Sector"].get(activity.dac_sector, ""), 
-        u'Sector (Agency specific)': cl_lookups["local-sectors"][activity.local_sector],
+        u'Sector (Agency specific)': activity.classification_data["mtef-sector"].codelist_code.name,
         u'Policy Markers': "",
         u'Collaboration Type': cl_lookups["CollaborationType"][activity.collaboration_type], 
         u'Default Flow Type': cl_lookups["FlowType"][activity.flow_type],
         u'Default Finance Type': cl_lookups["FinanceType"][activity.finance_type],
         u'Default Aid Type': cl_lookups["AidType"][activity.aid_type], 
         u'Default Tied Aid Status': cl_lookups["TiedStatus"][activity.tied_status],
-        u'Activity Budget': sum_commitments,
+        u'Activity Budget': "",
         u'Planned Disbursements': "", 
-        u'Financial transaction (Commitment)': "", 
+        u'Financial transaction (Commitment)': sum_commitments, 
         u'Financial transaction (Disbursement & Expenditure)': sum_disbursements, 
         u'Financial transaction (Reimbursement)': "", 
         u'Financial transaction (Incoming Funds)': "", 
         u'Financial transaction (Loan repayment / interest repayment)': "", 
         u'Activity Documents': "", 
         u'Activity Website': "", 
-        u'Related Activity': "", 
-        u'Conditions attached Y/N': "",
-        u'Text of Conditions': "",
-        u'Results data': "", 
-        u'Cofinancement': u"Non",
-        u'Date de mise à jour du projet': activity.updated_date.date().isoformat(), 
-        u'Date de la dernière publication': datetime.datetime.now().date().isoformat(),
-        u'Date de publication NCO': "", 
-        u'CRG': ""}
+        u'Last updated date': activity.updated_date.date().isoformat()
+    }
 
 def generate_csv():
     csv_file = StringIO.StringIO()
@@ -104,8 +93,6 @@ def generate_csv():
     
     csv = unicodecsv.DictWriter(csv_file, headers)
     csv.writeheader()
-    fr_headers_row = dict(map(lambda x: (x[1], fr_headers[x[0]]), enumerate(headers)))
-    csv.writerow(fr_headers_row)
     
     activities = qactivity.list_activities()
     for activity in activities:
