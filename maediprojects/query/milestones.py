769a955f8d2150ba622ec6392143b8e80fd16185
Mark Brough <mark.brough@gmail.com>
2018-10-25 15:10:33 +0000

    Add functions to handle milestones

---
+++ maediprojects/query/milestones.py
@@ -0,0 +1,26 @@
+from maediprojects import db, models
+from sqlalchemy import *
+import datetime
+from maediprojects.lib.util import MONTHS_QUARTERS, QUARTERS_MONTH_DAY
+
+def isostring_date(value):
+    # Returns a date object from a string of format YYYY-MM-DD
+    return datetime.datetime.strptime(value, "%Y-%m-%d")
+
+def isostring_year(value):
+    # Returns a date object from a string of format YYYY
+    return datetime.datetime.strptime(value, "%Y")
+
+def add_or_update_activity_milestone(data):
+    checkM = models.ActivityMilestone.query.filter_by(
+        activity_id=data["activity_id"],
+        milestone_id=data["milestone_id"]
+        ).first()
+    if not checkM:
+        checkM = models.ActivityMilestone()
+        checkM.activity_id = data["activity_id"]
+        checkM.milestone_id = data["milestone_id"]
+    setattr(checkM, data["attribute"], data["value"])
+    db.session.add(checkM)
+    db.session.commit()
+    return True  
