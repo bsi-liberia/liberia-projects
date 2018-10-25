b326a2fea7b628da1d3139f8948b8a99fe9beca2
Mark Brough <mark.brough@gmail.com>
2018-10-25 12:43:55 +0000

    Convert periods to dates for import from IFMIS data

--- maediprojects/lib/util.py
+++ maediprojects/lib/util.py
@@ -24,6 +24,32 @@ LR_QUARTERS_MONTH_DAY = {
     2: {"start": (1, 10), "end": (31, 12)}
 }
 
+LR_PERIODS_MONTH_DAY = {
+    "M7": {"start": (1, 1), "end": (31, 1)},
+    "M8": {"start": (1, 2), "end": (28, 2)},
+    "M9": {"start": (1, 3), "end": (31, 3)},
+    "M10": {"start": (1, 4), "end": (30, 4)},
+    "M11": {"start": (1, 5), "end": (31, 5)},
+    "M12": {"start": (1, 6), "end": (30, 6)},
+    "M13": {"start": (1, 6), "end": (30, 6)},
+    "M0": {"start": (1, 7), "end": (31, 7)},
+    "M1": {"start": (1, 7), "end": (31, 7)},
+    "M2": {"start": (1, 8), "end": (31, 8)},
+    "M3": {"start": (1, 9), "end": (30, 9)},
+    "M4": {"start": (1, 10), "end": (31, 10)},
+    "M5": {"start": (1, 11), "end": (30, 11)},
+    "M6": {"start": (1, 12), "end": (31, 12)}
+}
+
+def fp_fy_to_date(fp, fy, start_end='start'):
+    """Convert from LR FP, FY to real date."""
+    if fp in ("M7","M8","M9","M10","M11","M12","M13"):
+        year = fy+1
+    else:
+        year = fy
+    day, month = LR_PERIODS_MONTH_DAY[fp][start_end]
+    return datetime.datetime(year, month, day)
+
 def fq_fy_to_date(fq, fy, start_end='start'):
     """Convert from LR FQ, FY to real date."""
     if fq in (3,4):
@@ -45,7 +71,7 @@ def date_to_fy_fq(date):
 def isostring_date(value):
     # Returns a date object from a string of format YYYY-MM-DD
     if value == "": return None
-    return datetime.datetime.strptime(value, "%Y-%m-%d")
+    return datetime.datetime.strptime(value[0:10], "%Y-%m-%d")
 
 def isostring_year(value):
     # Returns a date object from a string of format YYYY
