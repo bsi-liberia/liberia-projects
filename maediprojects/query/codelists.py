639e6c48c6ca07830813b383b8b6b96e8908ec8a
Mark Brough <mark.brough@gmail.com>
2018-10-25 12:49:14 +0000

    Improve handling of codelists

--- maediprojects/query/codelists.py
+++ maediprojects/query/codelists.py
@@ -2,6 +2,16 @@ from maediprojects import db, models
 import datetime
 import normality
 
+def update_activity_codelist(activitycodelistcode_id, data):
+    activity_codelist = models.ActivityCodelistCode.query.filter_by(
+        id = activitycodelistcode_id
+    ).first()
+    if not activity_codelist: return False
+    setattr(activity_codelist, data['attr'], data['value'])
+    db.session.add(activity_codelist)
+    db.session.commit()
+    return True
+
 def get_code_by_name(codelist, name):
     code = models.CodelistCode.query.filter_by(
         codelist_code = codelist,
@@ -30,30 +40,21 @@ def create_code(data):
 
 def update_attr(data):
     codelistcode = models.CodelistCode.query.filter_by(
-        code = data['code'],
+        id = data['id'],
     	codelist_code = data["codelist_code"]
     ).first()
     setattr(codelistcode, data['attr'], data['value'])
-    """
-    if data['codelist_code'] == 'funding-organisation':
-        models.Activity.query.filter_by(
-            funding_org_code
-        )
-    """
     db.session.add(codelistcode)
     db.session.commit()
-    return True
+    return codelistcode.id
 
 def delete_code(data):
-    check = models.Activity.query.filter((
-        models.Activity.executing_org == data['code']
-    ) | (
-        models.Activity.local_sector == data['code']
-    )).first()
+    check = models.ActivityCodelistCode.query.filter_by(
+        codelist_code_id=data['id']).first() or models.ActivityFinancesCodelistCode.query.filter_by(
+        codelist_code_id=data['id']).first()
     if check: return False
     codelistcode = models.CodelistCode.query.filter_by(
-        code = data['code'],
-    	codelist_code = data["codelist_code"]
+        id = data['id']
     ).first()
     db.session.delete(codelistcode)
     db.session.commit()
