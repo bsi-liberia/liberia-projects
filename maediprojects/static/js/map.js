f2eea0b3dc2168f554b967447a761bddca22805c
Mark Brough <mark.brough@gmail.com>
2018-10-25 13:37:58 +0000

    Improve map options to support display of projects on home page

--- maediprojects/static/js/map.js
+++ maediprojects/static/js/map.js
@@ -1,21 +1,27 @@
 var MAEDImap = {};
 var markers = {}
-var MAEDImap = function(elID) {
+var MAEDImap = function(elID, options={}) {
   var map, layerControl, year;
 
-  this.addLocation = function(data) {
+  this.addLocation = function(data, markerLayer=false, activity=false) {
     var newLocation = new L.marker(
       new L.LatLng(
         data['latitude'],
         data['longitude']
       )
     );
-    var popupContent = getPopupContent(data);
+    var popupContent = getPopupContent(data, activity, this.options);
     newLocation.bindPopup(popupContent);
-    map.addLayer(newLocation);
+    if (markerLayer) {
+      markerLayer.addLayer(newLocation)
+    } else {
+      map.addLayer(newLocation);
+    }
     markers[data.id] = newLocation;
   }
-  
+  this.addLayer = function(layer) {
+    map.addLayer(layer);
+  }
   this.removeLocation = function(data) {
     map.removeLayer(markers[data['id']]);
   }
@@ -30,7 +36,10 @@ var MAEDImap = function(elID) {
     }
   }
 
+  // Satellite map: markbrough.map-qmxr8jb5
+  // Non-Satellite map: https://d.tiles.mapbox.com/v3/markbrough.n3kod47p/{z}/{x}/{y}.png
   this._init = function () {
+    this.options = options;
     layer_MapBox = new L.tileLayer(
     'https://d.tiles.mapbox.com/v3/markbrough.n3kod47p/{z}/{x}/{y}.png',{
         maxZoom: 15, attribution: 'MapBox Streets'
@@ -43,7 +52,6 @@ var MAEDImap = function(elID) {
     layer_MapBox.addTo(map);
     map.scrollWheelZoom.disable();
   }
-  
   this.resize = function() {
     map.invalidateSize();
   }
@@ -52,11 +60,15 @@ var MAEDImap = function(elID) {
     map.fitBounds(bounds);
   }
 
-  function getPopupContent(data) {
-    var pc = data['name'];
-    return pc;
+  function getPopupContent(data, activity, options) {
+    if ("all_projects" in options) {
+      var pc = '<b>' + data['name'] + '</b><br /><a href="' + activity['url'] + '">' + activity['title']+'</a>';
+      return pc;
+    } else {
+      var pc = data['name'];
+      return pc;
+    }
   }
-  
   function slugify(text)
   {
     return text.toString().toLowerCase()
