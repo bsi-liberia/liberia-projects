2760bae715c02bb93ad9984ee1afbe9c940bba21
Mark Brough <mark.brough@gmail.com>
2018-10-25 12:33:43 +0000

    Adjust ordering of codelists and add new function to get lookups by code

--- maediprojects/lib/codelists.py
+++ maediprojects/lib/codelists.py
@@ -11,7 +11,7 @@ def get_db_codelists():
     # Get codelist name
     # { codelist: cdata }
     codelist_data = models.CodelistCode.query.order_by(
-        models.CodelistCode.code
+        models.CodelistCode.name
     ).all()
     codelists = dict(map(lambda k: (k.codelist_code, []), codelist_data))
     for cd in codelist_data:
@@ -39,7 +39,7 @@ def get_codelists():
         csv = unicodecsv.DictReader(cl_file)
         for row in csv:
             out[cl_name].append({"code": row["code"], 
-                "id": row.get("id"),
+                "id": row.get("id", row.get("code")),
                 "name": row["name_%s" % LANG]})
     out.update(get_db_codelists())
     return out
@@ -51,6 +51,15 @@ def get_codelists_lookups():
         codelists[cl] = {}
         for cl_item in cl_items:
             codelists[cl][cl_item["code"]] = cl_item["name"]
+    return codelists
+
+def get_codelists_lookups_by_code():
+    codelists = {}
+    in_codelists = get_codelists()
+    for cl, cl_items in in_codelists.items():
+        codelists[cl] = {}
+        for cl_item in cl_items:
+            codelists[cl][cl_item["code"]] = cl_item["id"]
     return codelists
 
 def get_codelists_lookups_by_name():
