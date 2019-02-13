bsi-liberia/liberia-projects

commit ed004040333536eeb5c483f547e1291a27751ce3

Move setup routes to click commands; update README

maediprojects/lib/codelists.py
@@ -22,8 +22,7 @@ def get_db_codelists():
         })
     return codelists
 
-def get_codelists():
-    LANG = babel.get_locale()
+def get_codelists(LANG=babel.get_locale()):
     current_dir = os.path.join(os.path.dirname(__file__))
     
     def only_csv(filename):

maediprojects/__init__.py
@@ -7,8 +7,20 @@ from flask_login import login_required, current_user
 import os
 import mistune
 
+from . import commands
+
+
+def register_commands(app):
+    """Register Click commands."""
+    app.cli.add_command(commands.setup)
+    app.cli.add_command(commands.setup_country)
+    app.cli.add_command(commands.import_liberia)
+    app.cli.add_command(commands.import_psip)
+
+
 app = Flask(__name__.split('.')[0])
 app.config.from_pyfile(os.path.join('..', 'config.py'))
+register_commands(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 babel = Babel(app)

maediprojects/commands.py
@@ -1,0 +1,34 @@
+"""Click commands."""
+import click
+
+
+@click.command()
+@click.argument('language', default='en')
+def setup(language):
+    """Initial setup."""
+    from query import setup as qsetup
+    qsetup.create_codes_codelists()
+    qsetup.import_countries(language)
+    qsetup.create_user()
+
+
+@click.command()
+@click.argument('country_code')
+def setup_country(country_code):
+    """Setup a country."""
+    from query import setup as qsetup
+    qsetup.import_countries(country_code)
+
+
+@click.command()
+def import_liberia():
+    """Import Liberia data."""
+    from query import import_liberia_db as qlibimport
+    qlibimport.import_file()
+
+
+@click.command()
+def import_psip():
+    """Import psip data."""
+    from query import import_psip as qpsipimport
+    qpsipimport.import_file()

maediprojects/query/setup.py
@@ -9,14 +9,11 @@ import unicodecsv
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-def setup():
-    db.create_all()
-    create_codes_codelists()
-    import_countries()
-    create_user()
 
-def import_countries():
-    countries = codelists.get_codelists()["Country"]
+def import_countries(language, country_code=None):
+    countries = codelists.get_codelists(language)["Country"]
+    if country_code is not None:
+        countries = [c for c in countries if c["code"] == country_code]
     for country in countries:
         if country["code"] == "": continue
         c = models.Country()

maediprojects/routes.py
@@ -5,9 +5,6 @@ from maediprojects import app, db
 import models
 from views import activities, api, users, codelists, documentation
 from query import activity as qactivity
-from query import setup as qsetup
-from query import import_liberia_db as qlibimport
-from query import import_psip as qpsipimport
 import os
 import mistune
 
@@ -27,25 +24,6 @@ def setup_default_permissions():
         else:
             session["permissions"]["domestic_external"] = "both"
 
-# @app.route("/setup/")
-# def setup():
-#     qsetup.setup()
-#     return "OK"
-
-# @app.route("/setup/<country_code>/")
-# def setup_country(country_code):
-#     qsetup.import_locations(country_code)
-#     return "OK"
-
-# @app.route("/import_liberia/")
-# def import_liberia():
-#     qlibimport.import_file()
-#     return "OK"
-
-# @app.route("/import_psip/")
-# def import_psip():
-#     qpsipimport.import_file()
-#     return "OK"
 
 @app.errorhandler(404)
 def page_not_found(error):

commit c5e4344548d13b87365fb33829498504c05afd2b

Set default LANG correctly
Fixes error introduced in ed004040.

maediprojects/lib/codelists.py
@@ -22,7 +22,10 @@ def get_db_codelists():
         })
     return codelists
 
-def get_codelists(LANG=babel.get_locale()):
+def get_codelists(LANG=None):
+    if not LANG:
+        LANG = babel.get_locale()
+
     current_dir = os.path.join(os.path.dirname(__file__))
     
     def only_csv(filename):