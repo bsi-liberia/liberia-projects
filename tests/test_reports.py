26e3e7643987a16ac2cdb7fd8c553535c313fdae
Mark Brough <mark.brough@gmail.com>
2019-04-02 19:42:38 +0200

    Move reports tests to their own file

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -4,7 +4,8 @@ from flask import url_for
 class TestDocumentation:
     def test_nonauth_routes_work(self, client):
         routes = [
-            (url_for('documentation.help'), 302),
+            (url_for('reports.milestones'), 302),
+            (url_for('reports.disbursements_dashboard'), 302),
         ]
         for route, status_code in routes:
             res = client.get(route)
@@ -12,7 +13,9 @@ class TestDocumentation:
 
     def test_auth_routes_work(self, client, user):
         routes = [
-            (url_for('documentation.help'), 200),
+            (url_for('reports.milestones'), 200),
+            (url_for('reports.disbursements_dashboard'), 200),
+            (url_for('reports.counterpart_funding'), 200),
         ]
         for route, status_code in routes:
             res = client.get(route)
