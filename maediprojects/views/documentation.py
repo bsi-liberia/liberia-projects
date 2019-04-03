1bab3a2bc53b69d30d16c848c50ebdbc45dddd2f
Mark Brough <mark.brough@gmail.com>
2019-04-03 20:00:29 +0200

    Use python-markdown rather than mistune as it has TOC and table support

--- maediprojects/views/documentation.py
+++ maediprojects/views/documentation.py
@@ -5,7 +5,6 @@ import functools
 from flask import Blueprint, Flask, render_template, flash, request, Markup, \
     session, redirect, url_for, escape, Response, abort, send_file, jsonify
 from flask_login import login_required, current_user
-import mistune
 
 from maediprojects import models
 from maediprojects.extensions import db
@@ -13,6 +12,10 @@ from maediprojects.views import activities, api, users, codelists
 from maediprojects.query import activity as qactivity
 from maediprojects.lib import util
 
+import re
+import markdown
+from markdown.extensions.toc import TocExtension
+from markdown.extensions.tables import TableExtension
 
 blueprint = Blueprint('documentation', __name__, url_prefix='/', static_folder='../static')
 
@@ -22,7 +25,11 @@ blueprint = Blueprint('documentation', __name__, url_prefix='/', static_folder='
 def help():
     current_dir = os.path.join(os.path.dirname(__file__))
     help_content = open(os.path.join(current_dir, "../templates/help.md"), "r").read()
+
+    output_text = re.sub("<table>", '<table class="table">', markdown.markdown(help_content,
+        extensions=[TocExtension(baselevel=1,
+            title="Overview", toc_depth="2-3"), TableExtension()]))
     return render_template("help.html",
-                markdown_text = Markup(mistune.markdown(help_content)),
+                markdown_text = Markup(output_text),
                 loggedinuser=current_user
         )
