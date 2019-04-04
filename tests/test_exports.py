20090b1ea3546301fdd09ce34531882c378c9054
Mark Brough <mark.brough@gmail.com>
2019-04-04 18:01:34 +0200

    Move export tests to separate file

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -4,12 +4,12 @@ from flask import url_for
 class TestAPI:
     def test_nonauth_routes_work(self, client):
         routes = [
-            (url_for('api.api_activities_country'), 200),
-            (url_for('api.api_list_routes'), 200),
-            (url_for('api.api_list_iati_files'), 200),
-            (url_for('api.api_sectors'), 200),
-            (url_for('api.api_sectors_C_D'), 200),
-            (url_for('api.api_all_activity_locations'), 302),
+            (url_for('exports.activities_csv'), 200),
+            (url_for('exports.activities_xlsx_transactions'), 200),
+            (url_for('exports.activities_xlsx'), 200),
+            (url_for('exports.all_activities_xlsx'), 200),
+            (url_for('exports.all_activities_xlsx_filtered'), 200),
+            (url_for('exports.export'), 200),
         ]
         for route, status_code in routes:
             res = client.get(route)
@@ -17,7 +17,6 @@ class TestAPI:
 
     def test_auth_routes_work(self, client, user):
         routes = [
-            (url_for('api.api_all_activity_locations'), 200),
         ]
         for route, status_code in routes:
             res = client.get(route)
