ea635ed3a396ead702df004055309d27b782a30e
Andy Lulham <a.lulham@gmail.com>
2019-04-04 11:16:51 +0100

    Remove unused imports

--- maediprojects/views/reports.py
+++ maediprojects/views/reports.py
@@ -1,15 +1,10 @@
-import os
 import datetime
 import functools
 
-from flask import Blueprint, Flask, render_template, flash, request, Markup, \
-    session, redirect, url_for, escape, Response, abort, send_file, jsonify
+from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
 from maediprojects import models
-from maediprojects.extensions import db
-from maediprojects.views import activities, api, users, codelists
-from maediprojects.query import activity as qactivity
 from maediprojects.lib import util
 
 
