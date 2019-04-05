e3b557c23f15dfa99e232349e41d2d1bc8f0efa8
Andy Lulham <a.lulham@gmail.com>
2019-04-05 10:53:17 +0100

    Linting

--- maediprojects/query/counterpart_funding.py
+++ maediprojects/query/counterpart_funding.py
@@ -3,17 +3,18 @@ from sqlalchemy import *
 import datetime
 from maediprojects import models
 from maediprojects.extensions import db
-from maediprojects.lib import util
-from maediprojects.lib.util import MONTHS_QUARTERS, QUARTERS_MONTH_DAY
 import activity as qactivity
+
 
 def isostring_date(value):
     # Returns a date object from a string of format YYYY-MM-DD
     return datetime.datetime.strptime(value, "%Y-%m-%d")
 
+
 def isostring_year(value):
     # Returns a date object from a string of format YYYY
     return datetime.datetime.strptime(value, "%Y")
+
 
 def add_entry(activity_id, data):
     CF = models.ActivityCounterpartFunding()
@@ -24,44 +25,47 @@ def add_entry(activity_id, data):
     db.session.add(CF)
     db.session.commit()
 
-    qactivity.activity_updated(activity_id,
+    qactivity.activity_updated(
+        activity_id,
         {
-        "user_id": current_user.id,
-        "mode": "add",
-        "target": "ActivityCounterpartFunding",
-        "target_id": CF.id,
-        "old_value": None,
-        "value": CF.as_dict()
+            "user_id": current_user.id,
+            "mode": "add",
+            "target": "ActivityCounterpartFunding",
+            "target_id": CF.id,
+            "old_value": None,
+            "value": CF.as_dict()
         }
-        )
+    )
     return CF.id
+
 
 def delete_entry(activity_id, counterpartfunding_id):
     checkCF = models.ActivityCounterpartFunding.query.filter_by(
-        activity_id = activity_id,
-        id = counterpartfunding_id
+        activity_id=activity_id,
+        id=counterpartfunding_id
     ).first()
     if checkCF:
         old_value = checkCF.as_dict()
         db.session.delete(checkCF)
         db.session.commit()
-        qactivity.activity_updated(checkCF.activity_id,
+        qactivity.activity_updated(
+            checkCF.activity_id,
             {
-            "user_id": current_user.id,
-            "mode": "delete",
-            "target": "ActivityCounterpartFunding",
-            "target_id": old_value["id"],
-            "old_value": old_value,
-            "value": None
+                "user_id": current_user.id,
+                "mode": "delete",
+                "target": "ActivityCounterpartFunding",
+                "target_id": old_value["id"],
+                "old_value": old_value,
+                "value": None
             }
-            )
+        )
         return True
     return False
 
 # Counterpart funding data
 def update_entry(data):
     cf = models.ActivityCounterpartFunding.query.filter_by(
-        id = data['id']
+        id=data['id']
     ).first()
     old_value = getattr(cf, data['attr'])
     if data['attr'].endswith('date'):
@@ -76,18 +80,20 @@ def update_entry(data):
     db.session.add(cf)
     db.session.commit()
 
-    qactivity.activity_updated(cf.activity_id,
+    qactivity.activity_updated(
+        cf.activity_id,
         {
-        "user_id": current_user.id,
-        "mode": "update",
-        "target": "ActivityCounterpartFunding",
-        "target_id": cf.id,
-        "old_value": {data['attr']: old_value},
-        "value": {data['attr']: data['value']}
+            "user_id": current_user.id,
+            "mode": "update",
+            "target": "ActivityCounterpartFunding",
+            "target_id": cf.id,
+            "old_value": {data['attr']: old_value},
+            "value": {data['attr']: data['value']}
         }
-        )
+    )
 
     return True
+
 
 def create_or_update_counterpart_funding(activity_id, required_date, value):
     """
@@ -102,22 +108,23 @@ def create_or_update_counterpart_funding(activity_id, required_date, value):
     """
     print(required_date)
     cf = models.ActivityCounterpartFunding.query.filter_by(
-        activity_id = activity_id,
-        required_date = required_date
+        activity_id=activity_id,
+        required_date=required_date
     ).first()
     # If 0, we just remove the row from database
     if cf and value == 0:
         old_value = cf.as_dict()
         db.session.delete(cf)
         db.session.commit()
-        qactivity.activity_updated(cf.activity_id,
+        qactivity.activity_updated(
+            cf.activity_id,
             {
-            "user_id": current_user.id,
-            "mode": "delete",
-            "target": "ActivityCounterpartFunding",
-            "target_id": cf.id,
-            "old_value": old_value,
-            "value": None
+                "user_id": current_user.id,
+                "mode": "delete",
+                "target": "ActivityCounterpartFunding",
+                "target_id": cf.id,
+                "old_value": old_value,
+                "value": None
             }
         )
         return True
@@ -135,14 +142,15 @@ def create_or_update_counterpart_funding(activity_id, required_date, value):
     cf.required_value = value
     db.session.add(cf)
     db.session.commit()
-    qactivity.activity_updated(cf.activity_id,
-            {
+    qactivity.activity_updated(
+        cf.activity_id,
+        {
             "user_id": current_user.id,
             "mode": mode,
             "target": "ActivityCounterpartFunding",
             "target_id": cf.id,
             "old_value": old_value,
             "value": cf.as_dict()
-            }
-        )
+        }
+    )
     return True
