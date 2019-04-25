47b99ef8e5c3a855464440dbfd4f50f4a85a1398
Mark Brough <mark.brough@gmail.com>
2019-04-25 22:49:25 +0000

    Some improvements to performance of aid disbursements template and addition of PSIP template

--- maediprojects/views/reports.py
+++ maediprojects/views/reports.py
@@ -49,12 +49,10 @@ def counterpart_funding():
     )
 
 
-@blueprint.route("/reports/disbursements/")
-@blueprint.route("/reports/disbursements/<visualisation_type>")
+@blueprint.route("/reports/disbursements/aid/")
 @login_required
-def disbursements_dashboard(visualisation_type='forwardspends'):
+def aid_disbursements():
     def filter_relevant(finances, transaction_type=u'D'):
-        print(transaction_type)
         year_end = datetime.date(2019, 6, 30)
         year_start = datetime.date(2018, 7, 1)
         if hasattr(finances, 'transaction_date'):
@@ -76,12 +74,13 @@ def disbursements_dashboard(visualisation_type='forwardspends'):
     # Show actual disbursement as % of total
     # Show time as % of total
     # Show in-year disbursement as % of mtef projections
-    activities = models.Activity.query.all()
+    activities = models.Activity.query.filter_by(
+        domestic_external=u"external").all()
 
     out = []
 
     for activity in activities:
-        sum_disbursements = sum(map(lambda l: l.transaction_value, filter(functools.partial(filter_relevant, transaction_type=u"D"), activity.finances)))
+        sum_disbursements = sum(map(lambda l: l.transaction_value, filter(functools.partial(filter_relevant, transaction_type=u"D"), activity.disbursements)))
         #sum_forwardspends = sum(map(lambda l: l.transaction_value, filter(functools.partial(filter_relevant, transaction_type=u"C"), activity.finances)))
         sum_forwardspends = sum(map(lambda l: l.value, filter(functools.partial(filter_relevant, transaction_type=u"D"), activity.forwardspends)))
 
@@ -97,10 +96,84 @@ def disbursements_dashboard(visualisation_type='forwardspends'):
     days_since_fy_begin = ((datetime.datetime.utcnow()-start_of_fy).days)
     progress_time = round(days_since_fy_begin/365.0*100.0, 2)
     return render_template(
-        "disbursements.html",
+        "aid_disbursements.html",
         activities=out,
         progress_time=progress_time,
         days_since_fy_begin=days_since_fy_begin,
         fy_start_day=datetime.datetime.strftime(start_of_fy, "%d.%m.%Y"),
         loggedinuser=current_user
     )
+
+@blueprint.route("/reports/disbursements/psip/")
+@login_required
+def psip_disbursements():
+    def filter_relevant(finances, transaction_type=u'D'):
+        year_end = datetime.date(2019,06,30)
+        year_start = datetime.date(2018,07,01)
+        if hasattr(finances, 'transaction_date'):
+            return bool((finances.transaction_date <= year_end) and
+                (finances.transaction_date >= year_start) and
+                (finances.transaction_type==transaction_type))
+        elif hasattr(finances, 'period_end_date'):
+            return bool((finances.period_end_date <= year_end) and
+                (finances.period_end_date >= year_start))
+
+    def make_pct(value1, value2):
+        try:
+            return round((float(value1)/float(value2))*100.0, 2)
+        except Exception:
+            return 0.0
+
+    # Show actual disbursement as % of total
+    # Show time as % of total
+    # Show in-year disbursement as % of mtef projections
+    activities = models.Activity.query.filter_by(
+        domestic_external=u"domestic").all()
+
+    out = []
+
+
+    options = {
+        u"commitments": u"Appropriation",
+        u"disbursements": u"Actual Disbursement",
+        u"allotments": u"Allotment",
+       #u"forwardspends": u"Planned Disbursements"
+    }
+    denominator = "commitments"
+    numerator = "disbursements"
+    cutoff = "commitments" #denominator
+    cutoff_amount = 0 # in millions
+
+    for activity in activities:
+        aggs = {
+            "disbursements": sum(map(lambda l: l.transaction_value, filter(functools.partial(filter_relevant, transaction_type=u"D"), activity.disbursements))),
+            "commitments": sum(map(lambda l: l.transaction_value, filter(functools.partial(filter_relevant, transaction_type=u"C"), activity.commitments))),
+            "allotments": sum(map(lambda l: l.transaction_value, filter(functools.partial(filter_relevant, transaction_type=u"99-A"), activity.allotments)))
+            #"forwardspends": sum_forwardspends = sum(map(lambda l: l.value, filter(functools.partial(filter_relevant, transaction_type=u"D"), activity.forwardspends)))
+        }
+        sum_numerator = aggs[numerator]
+        sum_denominator = aggs[denominator]
+        pct = make_pct(sum_numerator,sum_denominator)
+        if (aggs[cutoff]>(cutoff_amount*1000000)):
+            act = activity.as_jsonable_dict()
+            act['sum_numerator'] = "{:,.2f}".format(sum_numerator),
+            act['sum_denominator'] = "{:,.2f}".format(sum_denominator),
+            act['pct_difference'] = pct
+            out.append(act)
+    current_year, current_quarter = util.date_to_fy_fq(datetime.datetime.utcnow())
+    start_of_fy = util.fq_fy_to_date(1, current_year, start_end='start')
+    days_since_fy_begin = ((datetime.datetime.utcnow()-start_of_fy).days)
+    progress_time = round(days_since_fy_begin/365.0*100.0, 2)
+
+
+    return render_template("psip_disbursements.html",
+                activities = out,
+                progress_time = progress_time,
+                days_since_fy_begin = days_since_fy_begin,
+                fy_start_day = datetime.datetime.strftime(start_of_fy, "%d.%m.%Y"),
+                loggedinuser = current_user,
+                numerator = options[numerator],
+                denominator = options[denominator],
+                cutoff = options[cutoff],
+                cutoff_amount = cutoff_amount
+        )
