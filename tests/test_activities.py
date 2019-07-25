8b311b51fa8b98b6e2119ca8ba87b82642abe2ce
Mark Brough <mark.brough@gmail.com>
2019-07-25 23:17:00 +0200

    Using flask-pytest client_class fixture and first selenium test

--- tests/test_activities.py
+++ tests/test_activities.py
@@ -1,23 +1,41 @@
 from flask import url_for
+import pytest
+import warnings
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-
+@pytest.mark.usefixtures('client_class')
 class TestActivities:
-    def test_nonauth_routes_work(self, client):
+    def test_nonauth_routes_work(self):
         routes = [
             (url_for('activities.dashboard'), 302),
             (url_for('activities.activities'), 302),
             (url_for('activities.activity_new'), 302),
         ]
         for route, status_code in routes:
-            res = client.get(route)
+            res = self.client.get(route)
             assert res.status_code == status_code
 
-    def test_auth_routes_work(self, client, user):
+
+    def test_auth_routes_work(self, user):
         routes = [
             (url_for('activities.dashboard'), 200),
             (url_for('activities.activities'), 200),
             (url_for('activities.activity_new'), 200),
         ]
         for route, status_code in routes:
-            res = client.get(route)
+            res = self.client.get(route)
             assert res.status_code == status_code
+
+
+@pytest.mark.usefixtures('live_server')
+@pytest.mark.usefixtures('client_class')
+class TestActivitiesLoad():
+    def test_activities_load(self, app, selenium, selenium_login):
+        selenium.get(url_for('activities.activities', _external=True))
+        WebDriverWait(selenium, 5).until(
+            EC.presence_of_element_located((By.ID, 'activities_count'))
+        )
+        assert selenium.find_element(By.ID, "activities_count").text == "10 found"
+        assert len(selenium.find_elements(By.XPATH, "//table/tbody/tr")) == 10
