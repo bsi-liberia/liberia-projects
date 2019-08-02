d7bf8992ab12118345c98021b1019f65556ef01b
Mark Brough <mark.brough@gmail.com>
2019-08-02 20:08:16 +0200

    Simple send mail script

---
+++ maediprojects/query/send_email.py
@@ -0,0 +1,15 @@
+from flask import request, current_app
+
+from flask_mail import Message
+from maediprojects.extensions import mail
+from maediprojects.query import activity as qactivity
+
+
+def send_async_email(message_recipient, message_subject, message_body):
+    with current_app.app_context():
+        msg = Message(
+            subject = message_subject,
+            recipients = [message_recipient],
+            body = message_body
+        )
+        mail.send(msg)
