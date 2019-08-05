5e2e1d95b89d2d587990531e4535ca108f268c93
Mark Brough <mark.brough@gmail.com>
2019-08-05 15:54:03 +0200

    Update days ago to account for weekends, when FRED etc don't publish new rates

--- tests/test_exchangerates.py
+++ tests/test_exchangerates.py
@@ -5,7 +5,7 @@ class TestExchangeRates:
     def test_import_exchange_rates(self, client):
         from maediprojects.query import exchangerates
         from maediprojects import models
-        day_before_yesterday = datetime.datetime.utcnow().date() - datetime.timedelta(days=2)
-        since_date = day_before_yesterday.isoformat()
+        several_days_ago = datetime.datetime.utcnow().date() - datetime.timedelta(days=5)
+        since_date = several_days_ago.isoformat()
         exchangerates.import_exchange_rates_from_url(False, since_date)
         assert(type(models.ExchangeRate.query.first()) == models.ExchangeRate)
