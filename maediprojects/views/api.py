33379886ba549067084dfcf33ed4ecf04c18be52
Mark Brough <mark.brough@gmail.com>
2019-08-13 20:56:54 +0200

    Add API routes for activity log

--- maediprojects/views/api.py
+++ maediprojects/views/api.py
@@ -44,6 +44,14 @@ def jsonify(*args, **kwargs):
     return current_app.response_class(json.dumps(dict(*args, **kwargs), cls=JSONEncoder),
         mimetype='application/json')
 
+
+@blueprint.route("/api/")
+def api():
+    return jsonify(
+        activities = url_for('api.api_activities_country', _external=True)
+    )
+
+
 @blueprint.route("/api/activities/")
 def api_activities_country():
     arguments = request.args.to_dict()
@@ -512,3 +520,97 @@ def generate_iati_xml(version, country_code):
         return Response(xml, mimetype='text/xml')
 
     return "ERROR: UNKNOWN VERSION"
+
+
+@blueprint.route("/api/activitylog.json")
+@login_required
+@quser.permissions_required("edit")
+def activity_log():
+    offset = (int(request.args.get("page", 1))-1)*10
+    count = models.ActivityLog.query.count()
+    user_id = request.args.get("user_id", None)
+    activitylogs = quser.activitylog(offset=offset,
+        user_id=user_id)
+    def simple_log(al):
+        return {
+            "id": al.id,
+            "user": {
+                "id": al.user_id,
+                "username": al.user.username,
+                "url": url_for("users.users_edit", user_id=al.id, _external=True)
+            },
+            "activity": {
+                "id": al.activity_id,
+                "title": al.activity.title,
+                "url": url_for("activities.activity", activity_id=al.activity_id, _external=True)
+            },
+            "mode": {
+                "id": al.mode,
+                "text": al.mode_text
+            },
+            "date": al.log_date.replace(microsecond=0).isoformat(),
+            "target": {
+                "id": al.target_id,
+                "text": al.target_text
+            }
+        }
+    return jsonify(
+        count = count,
+        items = list(map(lambda al: simple_log(al), activitylogs)))
+
+
+@blueprint.route("/api/activitylog/<int:activitylog_id>.json")
+@login_required
+@quser.permissions_required("edit")
+def activity_log_detail(activitylog_id):
+    al = quser.activitylog_detail(activitylog_id)
+    def get_object(target, target_text, id):
+        if not getattr(models, target).query.get(id):
+            return None, None
+        if target == "ActivityLocation":
+            return "Location", getattr(models, target).query.get(id).locations.as_dict()
+        elif target == "ActivityFinances":
+            return "Financial data", getattr(models, target).query.get(id).as_simple_dict()
+        elif target == "ActivityFinancesCodelistCode":
+            return "Financial data", getattr(models, target).query.get(id).activityfinances.as_simple_dict()
+        else:
+            return target_text.title(), getattr(models, target).query.get(id).as_dict()
+        return None, None
+
+    _obj_title, _obj = get_object(al.target, al.target_text, al.target_id)
+
+    return jsonify(data={
+        "id": al.id,
+        "user": {
+            "id": al.user_id,
+            "username": al.user.username,
+            "url": url_for("users.users_edit", user_id=al.id, _external=True)
+        },
+        "activity": {
+            "id": al.activity_id,
+            "title": al.activity.title,
+            "url": url_for("activities.activity", activity_id=al.activity_id, _external=True)
+        },
+        "mode": {
+            "id": al.mode,
+            "text": al.mode_text
+        },
+        "date": al.log_date.replace(microsecond=0).isoformat(),
+        "target": {
+            "id": al.target_id,
+            "text": al.target_text,
+            "obj_title": _obj_title,
+            "obj": _obj
+        },
+        "value": json.loads(al.value) if al.value else None,
+        "old_value": json.loads(al.old_value) if al.old_value else None
+    })
+
+
+@blueprint.route("/api/users.json")
+@login_required
+@quser.permissions_required("edit")
+def users():
+    _users = quser.user_id_username()
+    return jsonify(users=_users)
+
