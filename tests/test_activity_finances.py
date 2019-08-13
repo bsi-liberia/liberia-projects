18f383cd72679f7e273866da231eb9b9a0ada3dc
Mark Brough <mark.brough@gmail.com>
2019-08-13 22:14:33 +0200

    Make add new transaction function reusable

--- tests/test_activity_finances.py
+++ tests/test_activity_finances.py
@@ -9,6 +9,26 @@ import json
 import time
 from conftest import LiveServerClass
 
+
+def add_activity_finances(self, app, selenium, selenium_login):
+    selenium.get(url_for('activities.activity_edit', activity_id=1, _external=True))
+    WebDriverWait(selenium, 10).until(
+        EC.presence_of_element_located((By.ID, 'financesTab'))
+    )
+    selenium.find_element(By.ID, "financesTab").click()
+    selenium.find_element(By.CSS_SELECTOR, "#collapseDisbursements .addFinancial").click()
+    WebDriverWait(selenium, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '#financial-data-D table tbody tr:nth-child(2)'))
+    )
+    assert len(selenium.find_elements(By.CSS_SELECTOR, "#financial-data-D table tbody tr")) == 2
+    newrow = selenium.find_element(By.CSS_SELECTOR, "#financial-data-D table tbody tr:nth-child(2)")
+    newrow.find_element(By.NAME, "transaction_value_original").send_keys(Keys.BACKSPACE)
+    newrow.find_element(By.NAME, "transaction_value_original").send_keys("500.00")
+    newrow.find_element(By.NAME, "transaction_description").click()
+    time.sleep(1) # Wait one second for DB roundtrip
+    assert("success" in newrow.find_element(By.NAME, "transaction_value_original").find_element(By.XPATH, "..").find_element(By.XPATH, "..").get_attribute("class"))
+
+
 @pytest.mark.usefixtures('client_class')
 class TestActivity:
     def test_auth_routes_work(self, user):
@@ -39,19 +59,4 @@ class TestActivityLoads(LiveServerClass):
             ).get_attribute("value") == "100.00"
 
     def test_add_activity_finances(self, app, selenium, selenium_login):
-        selenium.get(url_for('activities.activity_edit', activity_id=1, _external=True))
-        WebDriverWait(selenium, 10).until(
-            EC.presence_of_element_located((By.ID, 'financesTab'))
-        )
-        selenium.find_element(By.ID, "financesTab").click()
-        selenium.find_element(By.CSS_SELECTOR, "#collapseDisbursements .addFinancial").click()
-        WebDriverWait(selenium, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, '#financial-data-D table tbody tr:nth-child(2)'))
-        )
-        assert len(selenium.find_elements(By.CSS_SELECTOR, "#financial-data-D table tbody tr")) == 2
-        newrow = selenium.find_element(By.CSS_SELECTOR, "#financial-data-D table tbody tr:nth-child(2)")
-        newrow.find_element(By.NAME, "transaction_value_original").send_keys(Keys.BACKSPACE)
-        newrow.find_element(By.NAME, "transaction_value_original").send_keys("500.00")
-        newrow.find_element(By.NAME, "transaction_description").click()
-        time.sleep(1) # Wait one second for DB roundtrip
-        assert("success" in newrow.find_element(By.NAME, "transaction_value_original").find_element(By.XPATH, "..").find_element(By.XPATH, "..").get_attribute("class"))
+        add_activity_finances(self, app, selenium, selenium_login)
