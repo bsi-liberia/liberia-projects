c836170bea715c5692e06d165f78cab7e53f01e2
Mark Brough <mark.brough@gmail.com>
2019-08-16 13:39:00 +0200

    Rename activity log to monitoring and add dataquality tests

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -14,7 +14,8 @@ import test_activity_finances
 class TestActivityLog:
     def test_auth_routes_work(self, user):
         routes = [
-            (url_for('api.activity_log'), 200)
+            (url_for('api.activity_log'), 200),
+            (url_for('api.reporting_orgs'), 200)
         ]
         for route, status_code in routes:
             res = self.client.get(route)
@@ -41,3 +42,22 @@ class TestActivityLogLoads(LiveServerClass):
         selenium.find_element(By.CSS_SELECTOR, '#usersLog tbody tr td:nth-child(5) a').click()
         time.sleep(1) # Wait one second for DB roundtrip
         assert("Activity" in selenium.find_element(By.CSS_SELECTOR, "#activityLogDetail header h5").text)
+
+
+@pytest.mark.usefixtures('client_class')
+class TestDataQualityLoads(LiveServerClass):
+
+    def test_data_quality_loads(self, app, selenium, selenium_login):
+        selenium.get(url_for('reports.dataquality', _external=True))
+        WebDriverWait(selenium, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#reportingOrgs tbody tr'))
+        )
+        assert len(selenium.find_elements(By.CSS_SELECTOR, '#reportingOrgs tbody tr')) == 1
+
+    def test_data_quality_correct(self, app, selenium, selenium_login):
+        selenium.get(url_for('reports.dataquality', _external=True))
+        WebDriverWait(selenium, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#reportingOrgs tbody tr'))
+        )
+        assert len(selenium.find_elements(By.CSS_SELECTOR, '#reportingOrgs tbody tr td i.fas.fa-check-circle.text-success')) == 1
+
