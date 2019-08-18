99fe2edefcfe5ba54bbc48b1c94970e4aa3d5327
Mark Brough <mark.brough@gmail.com>
2019-08-18 14:41:17 +0200

    Ensure that view-only user can see projects

--- tests/test_activities.py
+++ tests/test_activities.py
@@ -47,6 +47,22 @@ class TestActivities:
 
 @pytest.mark.usefixtures('client_class')
 class TestActivitiesLoad(LiveServerClass):
+    def test_activities_load_user(self, app, selenium, selenium_login_user):
+        selenium.get(url_for('activities.activities', _external=True))
+        try:
+            WebDriverWait(selenium, 10).until(
+                EC.presence_of_element_located((By.ID, 'activities_count'))
+            )
+        except TimeoutException as ex:
+            # print messages
+            print("----LOGS----")
+            for entry in selenium.get_log('browser'):
+                print(entry)
+            print("----LOGS----")
+            raise TimeoutException
+        assert selenium.find_element(By.ID, "activities_count").text == "10 found"
+        assert len(selenium.find_elements(By.XPATH, "//table/tbody/tr")) == 10
+
     def test_activities_load(self, app, selenium, selenium_login):
         selenium.get(url_for('activities.activities', _external=True))
         try:
