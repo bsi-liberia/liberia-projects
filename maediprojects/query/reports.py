82224785980752743ccf6624f97754e27bca5cfe
Mark Brough <mark.brough@gmail.com>
2019-08-21 21:27:46 +0000

    Move functions to query, rewrite

--- maediprojects/query/reports.py
+++ maediprojects/query/reports.py
@@ -6,9 +6,11 @@ import sqlalchemy as sa
 from sqlalchemy import func, case
 import datetime
 
-def make_forwardspends_disbursements_data(fiscal_year, forwardspends_over=1000000):
-    year_end = util.fq_fy_to_date(4, fiscal_year, "end")
-    year_start = util.fq_fy_to_date(1, fiscal_year, "start")
+
+def make_forwardspends_disbursements_data(fiscal_year, forwardspends_over=1000000,
+        domestic_external="external"):
+    year_end = util.fq_fy_to_date(4, fiscal_year, "end").date()
+    year_start = util.fq_fy_to_date(1, fiscal_year, "start").date()
 
     forwardspends = db.session.query(
         func.sum(models.ActivityForwardSpend.value).label("sum_forwardspends"),
@@ -19,7 +21,7 @@ def make_forwardspends_disbursements_data(fiscal_year, forwardspends_over=100000
         ).join(models.Activity, models.Activity.id == models.ActivityForwardSpend.activity_id
         ).join(models.Organisation, models.Activity.reporting_org
         ).filter(
-            models.Activity.domestic_external == 'external'
+            models.Activity.domestic_external == domestic_external
         ).filter(
             models.ActivityForwardSpend.period_end_date <= year_end
         ).filter(
@@ -61,6 +63,72 @@ def make_forwardspends_disbursements_data(fiscal_year, forwardspends_over=100000
             if disbursements.get(activity.id):
                 act["sum_disbursements"] = disbursements[activity.id].sum_disbursements
                 act["pct"] = make_pct(act["sum_disbursements"], activity.sum_forwardspends)
+            else:
+                act["sum_disbursements"] = 0.00
+                act["pct"] = 0.00
+            out.append(act)
+        return out
+    return _make_activity_data()
+
+
+def make_appropriations_disbursements_data(fiscal_year,
+        appropriations_over=0, domestic_external='domestic'):
+    year_start = util.fq_fy_to_date(1, fiscal_year, "start").date()
+    year_end = util.fq_fy_to_date(4, fiscal_year, "end").date()
+
+    appropriations = db.session.query(
+        func.sum(models.ActivityFinances.transaction_value).label("sum_appropriations"),
+        models.Activity.id,
+        models.Activity.title,
+        models.Activity.reporting_org_id,
+        models.Organisation.name.label("reporting_org_name")
+        ).join(models.Activity, models.Activity.id == models.ActivityFinances.activity_id
+        ).join(models.Organisation, models.Activity.reporting_org
+        ).filter(
+            models.Activity.domestic_external == domestic_external
+        ).filter(
+            models.ActivityFinances.transaction_date >= year_start
+        ).filter(
+            models.ActivityFinances.transaction_date <= year_end
+        ).filter(
+            models.ActivityFinances.transaction_type == u'C'
+        ).group_by(
+            models.Activity.id,
+            models.Activity.title,
+            models.Activity.reporting_org_id,
+            models.Organisation.name
+        ).order_by(func.sum(models.ActivityFinances.transaction_value).desc()
+        ).all()
+
+    disbursements = dict(map(lambda a: (a.id, a), db.session.query(
+        func.sum(models.ActivityFinances.transaction_value).label("sum_disbursements"),
+        models.Activity.id
+        ).join(models.Activity, models.Activity.id == models.ActivityFinances.activity_id
+        ).filter(
+            models.Activity.domestic_external == domestic_external
+        ).filter(
+            models.ActivityFinances.transaction_date <= year_end
+        ).filter(
+            models.ActivityFinances.transaction_date >= year_start
+        ).filter(
+            models.ActivityFinances.transaction_type == u'D'
+        ).group_by(
+            models.Activity.id
+        ).all()))
+
+    def make_pct(value1, value2):
+        try:
+            return round((float(value1)/float(value2))*100.0)
+        except Exception:
+            return None
+
+    def _make_activity_data(out=[]):
+        for activity in appropriations:
+            act = dict(zip(activity.keys(), activity))
+            act["url"] = url_for('activities.activity', activity_id=activity.id)
+            if disbursements.get(activity.id):
+                act["sum_disbursements"] = disbursements[activity.id].sum_disbursements
+                act["pct"] = make_pct(act["sum_disbursements"], activity.sum_appropriations)
             else:
                 act["sum_disbursements"] = 0.00
                 act["pct"] = 0.00
--- maediprojects/views/reports.py
+++ maediprojects/views/reports.py
@@ -1,12 +1,11 @@
 import datetime
 import functools
 
-from flask import Blueprint, render_template, redirect, url_for, jsonify, request
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 
 from maediprojects import models
 from maediprojects.query import counterpart_funding as qcounterpart_funding
-from maediprojects.query import reports as qreports
 from maediprojects.lib import util
 
 
@@ -72,20 +71,6 @@ def results():
     )
 
 
-@blueprint.route("/api/disbursements/aid/")
-@login_required
-def aid_disbursements_api():
-    fiscal_year = int(request.args.get("fiscal_year", 2018))
-    start_of_fy = util.fq_fy_to_date(1, fiscal_year, start_end='start')
-    days_since_fy_began = ((datetime.datetime.utcnow()-start_of_fy).days)
-    progress_time = min(round(days_since_fy_began/365.0*100.0, 2), 100.0)
-
-    return jsonify(activities=qreports.make_forwardspends_disbursements_data(fiscal_year),
-        progress_time=progress_time,
-        days_since_fy_began=days_since_fy_began,
-        fy_start_day=datetime.datetime.strftime(start_of_fy, "%d.%m.%Y"))
-
-
 @blueprint.route("/reports/disbursements/aid/")
 @login_required
 def aid_disbursements():
@@ -94,76 +79,11 @@ def aid_disbursements():
         loggedinuser=current_user
     )
 
+
 @blueprint.route("/reports/disbursements/psip/")
 @login_required
 def psip_disbursements():
-    def filter_relevant(finances, transaction_type=u'D'):
-        year_end = datetime.date(2019,06,30)
-        year_start = datetime.date(2018,07,01)
-        if hasattr(finances, 'transaction_date'):
-            return bool((finances.transaction_date <= year_end) and
-                (finances.transaction_date >= year_start) and
-                (finances.transaction_type==transaction_type))
-        elif hasattr(finances, 'period_end_date'):
-            return bool((finances.period_end_date <= year_end) and
-                (finances.period_end_date >= year_start))
-
-    def make_pct(value1, value2):
-        try:
-            return round((float(value1)/float(value2))*100.0, 2)
-        except Exception:
-            return 0.0
-
-    # Show actual disbursement as % of total
-    # Show time as % of total
-    # Show in-year disbursement as % of mtef projections
-    activities = models.Activity.query.filter_by(
-        domestic_external=u"domestic").all()
-
-    out = []
-
-
-    options = {
-        u"commitments": u"Appropriation",
-        u"disbursements": u"Actual Disbursement",
-        u"allotments": u"Allotment",
-       #u"forwardspends": u"Planned Disbursements"
-    }
-    denominator = "commitments"
-    numerator = "disbursements"
-    cutoff = "commitments" #denominator
-    cutoff_amount = 0 # in millions
-
-    for activity in activities:
-        aggs = {
-            "disbursements": sum(map(lambda l: l.transaction_value, filter(functools.partial(filter_relevant, transaction_type=u"D"), activity.disbursements))),
-            "commitments": sum(map(lambda l: l.transaction_value, filter(functools.partial(filter_relevant, transaction_type=u"C"), activity.commitments))),
-            "allotments": sum(map(lambda l: l.transaction_value, filter(functools.partial(filter_relevant, transaction_type=u"99-A"), activity.allotments)))
-            #"forwardspends": sum_forwardspends = sum(map(lambda l: l.value, filter(functools.partial(filter_relevant, transaction_type=u"D"), activity.forwardspends)))
-        }
-        sum_numerator = aggs[numerator]
-        sum_denominator = aggs[denominator]
-        pct = make_pct(sum_numerator,sum_denominator)
-        if (aggs[cutoff]>(cutoff_amount*1000000)):
-            act = activity.as_jsonable_dict()
-            act['sum_numerator'] = "{:,.2f}".format(sum_numerator),
-            act['sum_denominator'] = "{:,.2f}".format(sum_denominator),
-            act['pct_difference'] = pct
-            out.append(act)
-    current_year, current_quarter = util.date_to_fy_fq(datetime.datetime.utcnow())
-    start_of_fy = util.fq_fy_to_date(1, current_year, start_end='start')
-    days_since_fy_begin = ((datetime.datetime.utcnow()-start_of_fy).days)
-    progress_time = round(days_since_fy_begin/365.0*100.0, 2)
-
-
-    return render_template("reports/psip_disbursements.html",
-                activities = out,
-                progress_time = progress_time,
-                days_since_fy_begin = days_since_fy_begin,
-                fy_start_day = datetime.datetime.strftime(start_of_fy, "%d.%m.%Y"),
-                loggedinuser = current_user,
-                numerator = options[numerator],
-                denominator = options[denominator],
-                cutoff = options[cutoff],
-                cutoff_amount = cutoff_amount
-        )
+    return render_template(
+        "reports/psip_disbursements.html",
+        loggedinuser=current_user
+    )
