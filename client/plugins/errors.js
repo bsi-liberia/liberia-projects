d5ab13cf0b3127d797ee48d1916b3e04ded1e973
Mark Brough <mark.brough@gmail.com>
2020-08-31 19:53:48 +0200

    Add toasts on 404 errors

--- client/plugins/errors.js
+++ client/plugins/errors.js
@@ -16,6 +16,15 @@ export default function ({ $axios, app, redirect }) {
       .then(() => {
         redirect('/login')
       })
+      Promise.reject(error)
+    } else if ([404].includes(code)) {
+      app.router.app.$bvToast.toast(`This page could not be found.`, {
+          title: 'Error',
+          autoHideDelay: 10000,
+          solid: true,
+          variant: 'danger'
+        })
+        Promise.reject(error)
     } else if ([500].includes(code)) {
       app.router.app.$bvToast.toast(`Sorry, there was an unexpected server error. The error was: ${error}. Please try again, or contact support if this problem persists.`, {
           title: 'Error',
@@ -23,6 +32,7 @@ export default function ({ $axios, app, redirect }) {
           solid: true,
           variant: 'danger'
         })
+        Promise.reject(error)
     } else if (error.response == undefined) {
       app.router.app.$bvToast.toast(`Sorry, there was an error. It appears that you are not connected to the Internet.`, {
           title: 'Error',
@@ -30,6 +40,7 @@ export default function ({ $axios, app, redirect }) {
           solid: true,
           variant: 'danger'
         })
+        Promise.reject(error)
     }
   })
 }
