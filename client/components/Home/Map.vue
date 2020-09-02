9d71e2b688b287ee6cedfa33de6a682414fef59e
Mark Brough <mark.brough@gmail.com>
2020-09-03 01:49:16 +0200

    Move home page to nuxtjs

---
+++ client/components/Home/Map.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <template v-if="isBusy">
+      <b-row style="margin-bottom: 20px;">
+        <b-col md="12" class="text-center">
+          <b-spinner class="align-middle" label="Loading..."></b-spinner>
+          <strong>Loading...</strong>
+        </b-col>
+      </b-row>
+    </template>
+    <template v-else>
+      <div id="locationMap">
+        <client-only>
+          <l-map
+            ref="activityMap"
+            :options="mapOptions"
+            :dragging="dragging"
+            :tap="tap"
+            :maxZoom="maxZoom"
+            :bounds="bounds"
+            :zoom="zoom">
+            <l-tile-layer :url="url"></l-tile-layer>
+            <v-marker-cluster :options="{chunkedLoading: true, showCoverageOnHover: false}">
+              <l-marker :lat-lng="marker.latlng" v-for="marker in allLocations" v-bind:key="marker.locationID">
+                <l-popup>
+                  <b>{{ marker.name }}</b><br />
+                  <nuxt-link :to="{ name: 'activities-id', params: {id: marker.id }}">{{ marker.title }}</nuxt-link>
+                </l-popup>
+              </l-marker>
+            </v-marker-cluster>
+          </l-map>
+        </client-only>
+      </div>
+    </template>
+  </div>
+</template>
+<style scoped>
+#locationMap {
+  height: 400px;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      validation: null,
+      allLocations: [],
+      url: "https://d.tiles.mapbox.com/v3/markbrough.n3kod47p/{z}/{x}/{y}.png",
+      mapOptions: {
+        zoomSnap: 0.,
+        scrollWheelZoom: false
+      },
+      zoom: 5,
+      maxZoom: 15,
+      dragging: false,
+      tap: false,
+      isBusy: true
+    }
+  },
+  head() {
+    return {
+      link: [
+        {
+          rel: "stylesheet",
+          href:
+            "https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/MarkerCluster.Default.css"
+        },
+        {
+          rel: "stylesheet",
+          href:
+            "https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/MarkerCluster.css"
+        }
+      ]
+    };
+  },
+  mounted: function () {
+    // We fire resize if the page height/width has
+    // changed to ensure the map is corectly rendered
+    // -- see below.
+    //window.addEventListener('resize', this.resize)
+    this.setupLocations()
+  },
+  computed: {
+    bounds() {
+      if (this.allLocations.length == 0) {
+        return L.latLngBounds([
+          [ 4.38333, -11.3242 ],
+          [ 8.37583, -7.56658 ]
+        ])
+       }
+      var lats = this.allLocations.map(location => {
+        return location.latitude
+      })
+      var longs = this.allLocations.map(location => {
+        return location.longitude
+      })
+      return L.latLngBounds([
+          [Math.min.apply(null, lats), Math.min.apply(null, longs)],
+          [Math.max.apply(null, lats), Math.max.apply(null, longs)]
+      ]);
+    },
+  },
+  methods: {
+    resize() {
+      if ('activityMap' in this.$refs) {
+        this.$refs.activityMap.mapObject.invalidateSize()
+      }
+    },
+    setupLocations() {
+      this.$axios.get(`activity_locations/`)
+        .then(response => {
+          this.allLocations = response.data.locations
+        })
+        .finally(f => {
+          this.isBusy = false
+        })
+    }
+  }
+}
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,160 @@
+<template>
+  <div>
+    <b-row class="nopadding">
+      <b-col class="intro-message text-center">
+        <h1>Tracking development<br />
+          across Liberia</h1>
+      </b-col>
+    </b-row>
+    <b-container>
+      <b-row class="mt-5">
+        <b-col>
+          <h2>Activity locations</h2>
+          <home-map id="locationMap"></home-map>
+        </b-col>
+      </b-row>
+      <b-row class="mt-2 mb-2">
+        <b-col><hr />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <h2>Projected and Actual Disbursements by Sector</h2>
+          <b-row>
+            <b-col md="6">
+              <b-form-group
+                label="Fiscal year"
+                label-align="right"
+                label-cols-sm="3">
+                <b-select v-model="selectedFY"
+                :options="fyOptions"></b-select>
+              </b-form-group>
+            </b-col>
+          </b-row>
+          <home-bar-chart :selected-fy="selectedFY" /></home-bar-chart>
+        </b-col>
+      </b-row>
+      <b-row class="mt-2 mb-2">
+        <b-col><hr />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <h2>Planned / Actual Disbursements by Sector, over time</h2>
+          <b-row>
+            <b-col md="6">
+              <b-form-group
+                label="Display"
+                label-align="right"
+                label-cols-sm="3">
+                <b-select v-model="selectedPlannedActualDisbursements"
+                :options="plannedActualDisbursementOptions"></b-select>
+              </b-form-group>
+            </b-col>
+            <b-col md="6" class="text-right">
+              <b-form-radio-group
+                id="btn-radios-1"
+                v-model="plannedActualDisbursementsStacked"
+                :options="plannedActualDisbursementsStackedOptions"
+                buttons
+                name="radios-btn-default"
+              ></b-form-radio-group>
+            </b-col>
+          </b-row>
+          <home-line-chart
+            :value-field="selectedPlannedActualDisbursements"
+            :fy-options="fyOptions" :stacked="plannedActualDisbursementsStacked"/></home-line-chart>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<style scoped>
+.nopadding {
+  margin-right: 0px;
+}
+.nopadding .col {
+  padding-right: 0px;
+}
+.intro-message {
+  background: #313578;
+    background: -moz-linear-gradient(45deg,#313578 0%,#666666 100%);
+    background: -webkit-gradient(linear,left bottom,right top,color-stop(0%,#313578),color-stop(100%,#666666));
+    background: -webkit-linear-gradient(45deg,#313578 0%,#666666 100%);
+    background: -o-linear-gradient(45deg,#313578 0%,#666666 100%);
+    background: -ms-linear-gradient(45deg,#313578 0%,#666666 100%);
+    background: linear-gradient(45deg, #313578 0%, #666666 100%) repeat scroll 0 0 transparent;
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#313578',endColorstr='#666666',GradientType=1);
+    -webkit-box-shadow: inset 0 3px 7px rgba(0,0,0,.2),inset 0 -3px 7px rgba(0,0,0,.2);
+    -moz-box-shadow: inset 0 3px 7px rgba(0,0,0,.2),inset 0 -3px 7px rgba(0,0,0,.2);
+    box-shadow: inset 0 3px 7px rgba(0,0,0,.2),inset 0 -3px 7px rgba(0,0,0,.2);
+    padding: 50px 0 40px;
+    margin-top: none;
+    color: #ffffff;
+}
+.intro-message h1 {
+    color: #ffffff;
+    font-size: 50px;
+    text-shadow: 1px 1px 2px #333333;
+}
+.intro #locationMap {
+  width: 100%;
+  height: 400px;
+}
+</style>
+<script>
+import HomeMap from '~/components/Home/Map.vue'
+import HomeBarChart from '~/components/Home/BarChart.vue'
+import HomeLineChart from '~/components/Home/LineChart.vue'
+export default {
+  data() {
+    return {
+      isBusy: true,
+      selectedFY: '2019',
+      fyOptions: [],
+      selectedPlannedActualDisbursements: 'Disbursements',
+      plannedActualDisbursementsStacked: true,
+      plannedActualDisbursementsStackedOptions: [
+        {
+          'value': true,
+          'text': 'Area chart'
+        },
+        {
+          'value': false,
+          'text': 'Line chart'
+        }
+      ],
+      plannedActualDisbursementOptions: [
+        {
+          'value': 'Disbursements',
+          'text': 'Actual Disbursements'
+        },
+        {
+          'value': 'Disbursement Projection',
+          'text': 'Planned Disbursements'
+        }
+      ]
+    }
+  },
+  components: {
+    HomeMap,
+    HomeBarChart,
+    HomeLineChart
+  },
+  mounted: function() {
+    this.getFYs()
+  },
+  watch: {
+  },
+  methods: {
+    getFYs() {
+      this.$axios
+      .get(`filters/available_fys.json`)
+      .then(response => {
+        this.fyOptions = [{'value': null, 'text': 'All Fiscal Years'}].concat(response.data.fys)
+        this.selectedFY = response.data.current_fy
+      });
+    }
+  }
+}
+</script>
