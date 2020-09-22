8535564a61641e1950aaa0b589efef127d90898e
Mark Brough <mark.brough@gmail.com>
2020-09-22 17:32:20 +0200

    Update description of template

--- client/nuxt.config.tmpl.js
+++ client/nuxt.config.tmpl.js
@@ -13,7 +13,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'The Dashboard is used to collect, analyze, and report information about external assistance programs and projects.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
