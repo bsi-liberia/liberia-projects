a833f4d241c1bcf9d034f39803552adcfc5a5c07
Mark Brough <mark.brough@gmail.com>
2021-04-27 14:00:19 +0200

    Add some tests for forwardspends

---
+++ tests/test_activity_forwardspends.py
@@ -0,0 +1,28 @@
+from flask import url_for
+import pytest
+import json
+
+
+@pytest.mark.usefixtures('client_class')
+class TestActivityFinances:
+    def test_auth_routes_work_user(self, user, headers_user):
+        routes = [
+            (url_for('activity_forwardspends.api_activity_forwardspends', activity_id=1), 403),
+        ]
+        for route, status_code in routes:
+            res = self.client.get(route, headers=headers_user)
+            assert res.status_code == status_code
+
+
+    def test_auth_routes_work_admin(self, admin, headers_admin):
+        routes = [
+            (url_for('activity_forwardspends.api_activity_forwardspends', activity_id=1), 200),
+        ]
+        for route, status_code in routes:
+            res = self.client.get(route, headers=headers_admin)
+            assert res.status_code == status_code
+            data = json.loads(res.data)
+            assert len(data['forwardspends']) == 7
+            print(data)
+            assert data['forwardspends'][4]['year'] == 'FY1819'
+            assert data['forwardspends'][4]['total_value'] == 1000
