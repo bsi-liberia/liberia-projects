379cdd244e6d5aa1129dbe50209bdbbdf0d22d58
Mark Brough <mark.brough@gmail.com>
2021-08-12 18:07:53 +0200

    Import fiscal years

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@ from projectdashboard.query.setup import create_codes_codelists, import_countrie
 from werkzeug.datastructures import FileStorage
 from projectdashboard.query.generate_xlsx import xlsx_to_csv, import_xls, import_xls_mtef
 from projectdashboard.query import activity as qactivity
+from projectdashboard.query import admin as qadmin
 from projectdashboard.query.location import import_locations_from_file
 from flask_jwt_extended import (
     create_access_token
@@ -41,6 +42,12 @@ def app():
         admin_dict = app.config["ADMIN_USER"]
         admin = addUser(admin_dict)
         assert models.User.query.get(admin.id).username == admin_dict["username"]
+
+        fiscal_years = [{
+            'from': app.config['EARLIEST_DATE'].isoformat(),
+            'to': app.config['LATEST_DATE'].isoformat()
+        }]
+        qadmin.process_fiscal_years(fiscal_years)
 
         import_test_data(app, admin)
         yield app
