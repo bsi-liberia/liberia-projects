26bf5c3c405e2be81062df660c9e6782cbedd77a
Mark Brough <mark.brough@gmail.com>
2022-03-15 21:47:42 +0000

    Update sector and donor pages

--- client/pages/donors/_id/index.vue
+++ client/pages/donors/_id/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <Aggregates type="donors" label="Donor" />
+  <Aggregates agg-filter="reporting-org" label="Donor" :agg-filter-value="aggFilterValue" />
 </template>
 <script>
 import Aggregates from '~/components/Aggregates.vue'
 export default {
   components: {
     Aggregates
+  },
+  computed: {
+    aggFilterValue() {
+      return this.$route.params.id
+    }
   }
 }
 </script>
