cb83eaa4fb77499a041919973e5d6f708d7e4de8
Mark Brough <mark.brough@gmail.com>
2022-03-20 19:32:10 +0000

    Add checkbox for published

--- client/components/ActivityEditor/subcomponents/finances-checkbox.vue
+++ client/components/ActivityEditor/subcomponents/finances-checkbox.vue
@@ -1,16 +1,29 @@
 <template>
-  <b-form-group :state="validation">
-    <b-form-checkbox :state="validation"
-      :disabled="disabled"
-      v-model="_value">{{ label }}</b-form-checkbox>
-  </b-form-group>
+  <div>
+    <template v-if="labelColsSm">
+      <b-form-group :state="validation"
+        :label="label"
+        :label-cols-sm="labelColsSm">
+        <b-form-checkbox :state="validation"
+          :disabled="disabled"
+          v-model="_value" switch>{{ labels[_value] }}</b-form-checkbox>
+      </b-form-group>
+    </template>
+    <template v-else>
+      <b-form-group :state="validation">
+        <b-form-checkbox :state="validation"
+          :disabled="disabled"
+          v-model="_value">{{ label }}</b-form-checkbox>
+      </b-form-group>
+    </template>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return { validation: null }
   },
-  props: ["transaction", "name", "label", "value", "disabled"],
+  props: ["transaction", "labels", "label-cols-sm", "name", "label", "value", "disabled"],
   inject: ['updateFinances'],
   computed: {
     _value: {
