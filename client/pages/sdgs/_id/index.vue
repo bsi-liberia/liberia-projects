b5210635ec12db89e4416d27361d539729cd5b93
Mark Brough <mark.brough@gmail.com>
2022-07-25 19:40:39 +0000

    Add pages for SDGs

---
+++ client/pages/sdgs/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Aggregates agg-filter="sdg-goals" label="Sustainable Development Goals" :agg-filter-value="aggFilterValue" />
+</template>
+<script>
+import Aggregates from '~/components/Aggregates.vue'
+export default {
+  components: {
+    Aggregates
+  },
+  computed: {
+    aggFilterValue() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
