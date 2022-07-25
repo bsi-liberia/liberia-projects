e7cbf57c18ed370564fb3727c1d5bb44c92198e8
Mark Brough <mark.brough@gmail.com>
2022-07-25 19:40:26 +0000

    Add sector overview page

---
+++ client/pages/sectors/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Sectors</h1>
+    <hr />
+    <AggregatesTop
+      dimension="mtef-sector"
+      dimension-label="Sector" />
+  </div>
+</template>
+<script>
+import AggregatesTop from '~/components/AggregatesTop.vue'
+export default {
+  components: {
+    AggregatesTop
+  }
+}
+</script>
