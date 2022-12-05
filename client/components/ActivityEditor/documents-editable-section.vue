564f615e90fbc5e513c1df5eccec380d5e59cb23
Mark Brough <mark.brough@gmail.com>
2022-12-05 22:53:24 +0000

    Make documents deleteable

--- client/components/ActivityEditor/documents-editable-section.vue
+++ client/components/ActivityEditor/documents-editable-section.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-table responsive fixed
-      :fields="['title', 'type', 'date']"
+      :fields="['title', 'type', 'date', 'delete']"
       :items="documents">
       <template v-slot:cell(title)="data">
         <a :href="data.item.url">{{ data.item.title }}</a>
@@ -12,6 +12,12 @@
       </template>
       <template v-slot:cell(date)="data">
         {{ data.item.document_date || '' }}
+      </template>
+      <template v-slot:cell(delete)="data">
+        <b-button variant="link" class="text-danger"
+          size="sm" @click="deleteDocument(data)">
+          <font-awesome-icon :icon="['fa', 'trash-alt']" />
+        </b-button>
       </template>
     </b-table>
     <b-btn variant="success" v-b-modal.add-document-modal>Add document</b-btn>
@@ -59,6 +65,31 @@ export default {
         .get(this.api_routes.documents)
         .then((response) => {
           this.documents = response.data.documents
+        })
+    },
+    deleteDocument(documentData) {
+      this.$bvModal.msgBoxConfirm('Are you sure you want to delete this document? This action cannot be undone!', {
+        title: 'Confirm delete',
+        okVariant: 'danger',
+        okTitle: 'Confirm delete',
+        hideHeaderClose: false,
+        centered: true
+      })
+        .then(value => {
+          if (value) {
+            this.$axios.delete(`${this.api_routes.document}/${documentData.item.id}.json`)
+            .then(returndata => {
+              this.$delete(this.documents, documentData.index)
+              this.$root.$bvToast.toast(`That document was deleted from the system.`, {
+                title: 'Document deleted',
+                autoHideDelay: 3000,
+                variant: 'success'
+              })
+            });
+          }
+        })
+        .catch(err => {
+          alert("Sorry, there was an error, and that document couldn't be deleted.")
         })
     }
   }
